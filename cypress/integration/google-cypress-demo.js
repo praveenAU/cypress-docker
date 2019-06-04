17ba58fb59b0bbcf349a366b5d316675766509e4
Praveen P <praveen.prasann@gmail.com>
2019-06-04 15:10:27 +1000

    Some cleanup

--- cypress/integration/google-cypress-demo.js
+++ cypress/integration/google-cypress-demo.js
@@ -1,20 +1,10 @@
 describe('Cypress Google Demo', function() {
-  
+
   it("Google search", function() {
 
     cy.visit('https://google.com')
 
-    cy.get('[name="q"]').type('cypress io')
+    cy.title().should('eq', 'Google')
 
-    cy.get('input[name="btnK"]').click()
-
-    cy.title().should('eq', 'cypress io - Google Search')
-
-    cy.contains('Cypress.io: JavaScript End to End Testing Framework').click()
-
-    cy.url().should('eq', 'https://www.cypress.io/')
-
-    cy.title().should('eq', 'JavaScript End to End Testing Framework | Cypress.io')
-  
   })
 })
